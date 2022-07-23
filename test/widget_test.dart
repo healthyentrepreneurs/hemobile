7899dcff0374881e392441e8f4befa88a5825535
njovu <njovujsh@gmail.com>
2022-07-23 14:43:28 +0300

    check em

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -6,10 +6,10 @@
 // tree, read text, and verify that the values of widget properties are correct.
 
 import 'package:flutter/material.dart';
-import 'package:flutter_test/flutter_test.dart';
+// import 'package:flutter_test/flutter_test.dart';
 import 'package:nl_health_app/backup_main.dart';
 
-
+// Papa wemba
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
